206ca047242135ac7340c00e877da189b8630de5
Nityanand17 <nityanandswami2324@gmail.com>
2025-05-02 09:32:05 +0530

    Update project files and add new configuration files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,8 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
         />
+        <link rel="stylesheet" href="/styles.css" />
+        <link rel="stylesheet" href="/tailwind.css" />
       </head>
       <body className={`${inter.variable} ${montserrat.variable} ${raleway.variable} font-sans bg-gray-50 min-h-screen flex flex-col`}>
         <AuthProvider>
